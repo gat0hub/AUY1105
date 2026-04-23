9131f4c863fc6e777485a7c3f602edfdb320417c
BF <bran.figueroa@duocuc.cl>
2026-04-23 19:00:25 -0400

    Update AWS provider version in provider.tf

--- provider.tf
+++ provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      # Última versión mayor del proveedor (Serie 5.x)
+      
       version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1" # Puedes cambiarla a la de tu preferencia
+  region = "us-east-1" 
 }
